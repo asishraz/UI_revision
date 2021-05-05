41539c3055c7d4315a07f2446918e8b3d59a66aa
asishraz <iamasish3001@gmail.com>
2021-05-06 03:54:28 +0530

    displaying images

---
+++ Project_5/myscript.js
@@ -0,0 +1,39 @@
+const myFunction = () => {
+  document.getElementById("first").style.display = "block";
+  document.getElementById("second").style.display = "none";
+  document.getElementById("third").style.display = "none";
+  document.getElementById("forth").style.display = "none";
+  document.getElementById("fifth").style.display = "none";
+};
+
+const myFunction2 = () => {
+  document.getElementById("second").style.display = "block";
+  document.getElementById("first").style.display = "none";
+  document.getElementById("third").style.display = "none";
+  document.getElementById("forth").style.display = "none";
+  document.getElementById("fifth").style.display = "none";
+};
+
+const myFunction3 = () => {
+  document.getElementById("third").style.display = "block";
+  document.getElementById("first").style.display = "none";
+  document.getElementById("second").style.display = "none";
+  document.getElementById("forth").style.display = "none";
+  document.getElementById("fifth").style.display = "none";
+};
+
+const myFunction4 = () => {
+  document.getElementById("forth").style.display = "block";
+  document.getElementById("first").style.display = "none";
+  document.getElementById("third").style.display = "none";
+  document.getElementById("second").style.display = "none";
+  document.getElementById("fifth").style.display = "none";
+};
+
+const myFunction5 = () => {
+  document.getElementById("fifth").style.display = "block";
+  document.getElementById("first").style.display = "none";
+  document.getElementById("third").style.display = "none";
+  document.getElementById("forth").style.display = "none";
+  document.getElementById("second").style.display = "none";
+};
