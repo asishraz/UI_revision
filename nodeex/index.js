0b66118a37dab7f8af8675d3ff7464c573c41a25
asishraz <iamasish3001@gmail.com>
2021-05-13 18:09:20 +0530

    new file added

--- nodeex/index.js
+++ nodeex/index.js
@@ -1,6 +1,16 @@
-var xyz = require('http')
+var fs = require('fs')
 
-xyz.createServer(function(req,res) {
-    res.write("naveen reddy")
-    res.end()
-}).listen(3000)
+fs.readFile('app.js','utf8',function(err,data) {
+    console.log(data);
+    
+})
+
+
+fs.appendFile('app.js','console.log("hello there");',function(err) {
+    console.log("data saved");
+    
+})
+
+fs.unlink('new.js', function(err) {
+    console.log("deleted");
+})
